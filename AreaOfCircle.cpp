53b39d549eb47c363f6342b8ab47d501600af68b
Aryan90951 <161210598+Aryan90951@users.noreply.github.com>
2024-02-25 16:53:52 +0530

    Add files via upload

---
+++ AreaOfCircle.cpp
@@ -0,0 +1,9 @@
+// #include <iostream>
+// using namespace std;
+// int main() {
+//   int r;
+//   cout<<"Enter the radius of circle :" ;
+//   cin>>r;
+//   cout<<"The area of circle is : " ;
+//   cout<<3.14*r*r;
+// }
